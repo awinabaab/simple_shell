48ce27ddff110a07626f014a2e3f91d3c1b2482d
johnkenzzy <johnkenumeh@gmail.com>
2024-08-27 05:29:37 -0700

    Fixed a bug

--- exit.c
+++ exit.c
@@ -1,5 +1,6 @@
 #include "main.h"
 
+int is_num(char *str);
 
 /**
  * exit_shell - Handles the exit built-in command
@@ -17,10 +18,33 @@ int exit_shell(char **args)
 	{
 		if (args[1] == NULL)
 			status = 0;
-		else if ((strtol(args[1], &endptr, 10)) >= 0)
+		else if (is_num(args[1]) == 1 && (strtol(args[1], &endptr, 10)) >= 0)
 			status = strtol(args[1], &endptr, 10);
 		else
 			status = -1;
 	}
 	return (status);
+}
+
+
+/**
+ * is_num - Checks if a string is numeric
+ *
+ * @str: The string to be checked
+ *
+ * Return: 1 if all characters are digit, else 0
+ */
+int is_num(char *str)
+{
+	if (str == NULL || *str == '\0')
+		return (0);
+
+	while (*str)
+	{
+		if (!isdigit((unsigned char)*str))
+			return (0);
+		str++;
+	}
+
+	return (1);
 }
