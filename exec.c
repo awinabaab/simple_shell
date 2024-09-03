7ac4c5695e81f47213d9bf32f07284218e7ee3d9
johnkenzzy <johnkenumeh@gmail.com>
2024-09-03 02:21:46 -0700

    Betty styling

--- exec.c
+++ exec.c
@@ -3,9 +3,7 @@
 /**
  * execution - Executes a command using fork and execve
  *
- * @argv: The child command to be executed
  * @args: An array of child command arguments
- * @env: An array of environment string
  * @flname: The filname of the parent process
  * @pcount: child process count
  *
@@ -16,11 +14,10 @@ int execution(char **args, char *flname, int pcount)
 {
 	pid_t pid;
 	struct stat st;
-	int status;/*exit_stat = exit_shell(args);*/
-	/*char *cmd = find_cmd_in_path(argv);*/
+	int status;
 
 	if (args == NULL)
-	       return (2);	
+		return (2);
 	if (strcmp(args[0], "exit") == 0)
 	{
 		free(args);
